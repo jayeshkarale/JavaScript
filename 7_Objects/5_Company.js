5fe6d76ff32652db4d21dfc9a8d2325ee0493336
jayeshkarale <jayeshkarale@mail.com>
2026-09-25 22:31:04 +0530

    modified file

--- 7_Objects/5_Company.js
+++ 7_Objects/5_Company.js
@@ -1,41 +1,35 @@
-let company={
-    cid:1001,
-    location:"pune",
-    
-    employee:[
-        {
-        eid: 1,
-        ename:"jayesh k",
-        salary:52000,
-        company:{
-            cname:  "TCS Ltd",
-            address:[
-                {
-                    city: 'pune',
-                    area:'hinjewadi'
-                }
-            ]
-        }
-    },
+let company = {
+  cid: 1001,
+  cname: "Cognizant Pvt Ltd",
+  location: "Pune",
 
-    {
-        eid: 2,
-        ename:"jagdish",
-        salary:52000,
-        company: "TCS Ltd"
-    },
+  address: [
+    { city: "pune", area: "hinjewadi" },
+    { city: "nagpur", area: "mihan" },
+    { city: "mumbai", area: "dadar" }
+  ],
 
+  employee: [
     {
-        eid: 3,
-        ename:"jagdish",
-        salary:52000,
-        company: "TCS Ltd"
+      eid: 1,
+      ename: "jayesh k",
+      salary: 52000,
+      branch: "pune"      // which office/address this employee is based at
+    },
+    {
+      eid: 2,
+      ename: "harshada s",
+      salary: 38000,
+      branch: "nagpur"
     }
-    ]
-
-}
+  ]
+};
 
 console.log(company);
 console.log(company.employee[0]);
-console.log(company.employee[2].salary);
+console.log(company.employee[1].salary);
 
+// bonus: look up the employee's actual branch address
+let emp = company.employee[1];
+let empAddress = company.address.find(a => a.city === emp.branch);
+console.log(empAddress); // { city: 'nagpur', area: 'mihan' }
